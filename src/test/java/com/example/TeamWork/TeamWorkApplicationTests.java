f7321de17ecb848ecfbc14368e76f4a1f4f4a861
Home <zavalnyy_saveliy@mail.ru>
2024-06-20 23:33:21 +1000

    !

---
+++ src/test/java/com/example/TeamWork/TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
